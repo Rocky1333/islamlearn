a5bf1475f7be6a0edade657adb10e2d354241c83
alan <margiev1234@icloud.com>
2024-08-22 21:45:02 +0200

    21312

--- public/script.js
+++ public/script.js
@@ -182,8 +182,8 @@ fetch('https://islamlearn.vercel.app/', {
 .then(response => response.json())
 .then(data => {
     if (data.balance !== undefined && data.click !== undefined) {
-        userBalance = data.balance;
-        userClick = data.click;
+        userBalance = data.user.balance;
+        userClick = data.user.click;
 		const balance = document.querySelector('.balance');
 		balance.textContent = userBalance
     } else {
